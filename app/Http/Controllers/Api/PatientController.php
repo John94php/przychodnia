2a7175e6ef72cb72f584998e0bd7aa559b1b15e4
Johny <janzalesinski@gmail.com>
2020-09-01 11:24:06 +0200

    remove unnessesary line

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
-use App\Repositories\Tasks;
 use Illuminate\Http\Request;
 use App\Repositories\Patients;
 class PatientController extends Controller
