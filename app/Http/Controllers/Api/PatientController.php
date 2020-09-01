d3affd352943212430f8228f43f9eeeb68835520
Johny <janzalesinski@gmail.com>
2020-09-01 11:22:52 +0200

    update files

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -4,8 +4,49 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Repositories\Tasks;
 use Illuminate\Http\Request;
-
+use App\Repositories\Patients;
 class PatientController extends Controller
 {
     //
+    protected $patientRepository;
+    public function __construct()
+    {
+        $this->patientRepository = new Patients;
+    }
+    public function index() {
+        $patientsFetch
+        = $this->patientRepository->all();
+        if($patientsFetch->hasError()) {
+            return response()->json($patientsFetch->getItems(),500);
+        }
+        return response()->json($patientsFetch->getItems(),200);
+    }
+    public function store(Request $request) {
+        $patientsStore = $this->patientRepository->store($request->all());
+        if($patientsStore->hasError()) {
+            return response()->json($patientsStore->getItems(),500);
+        }
+        return response()->json($patientsStore->getItems(),201);
+    }
+    public function show($id) {
+        $patientFetch = $this->patientRepository->show($id);
+        if($patientFetch->hasError()) {
+            return response()->json($patientFetch->getItems(),500);
+        }
+        return response()->json($patientFetch->getItems(),200);
+    }
+    public function update($id,Request $request) {
+        $patientUpdate = $this->patientRepository->update($id,$request->all());
+        if($patientUpdate->hasError()) {
+            return response()->json($patientUpdate->getItems(),500);
+        }
+        return response()->json($patientUpdate->getItems(),200);
+    }
+    public function delete($id) {
+        $patientDelete = $this->patientRepository->delete($id);
+        if($patientDelete->hasError()) {
+            return response()->json($patientDelete->getItems(),500);
+        }
+        return response()->json($patientDelete->getItems(),200);
+    }
 }
